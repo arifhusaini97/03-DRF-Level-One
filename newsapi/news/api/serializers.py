720622c74564bab4185fbc6fb1101100fa127879
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-08-10 15:40:01 +0800

    APIView and ModelSerializer Class - with Simple Custom Validation

--- newsapi/news/api/serializers.py
+++ newsapi/news/api/serializers.py
@@ -1,34 +1,66 @@
+from datetime import datetime
+from django.utils.timesince import timesince
 from rest_framework import serializers
 from news.models import Article
 
 
-class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    author = serializers.CharField()
-    title = serializers.CharField()
-    description = serializers.CharField()
-    body = serializers.CharField()
-    location = serializers.CharField()
-    publication_date = serializers.DateField()
-    active = serializers.BooleanField()
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+class ArticleSerializer(serializers.ModelSerializer):
+    time_since_publication = serializers.SerializerMethodField()
 
-    def create(self, validated_data):
-        print(validated_data)
-        return Article.objects.create(**validated_data)
+    class Meta:
+        model = Article
+        exclude = ("id",)
+        # fields = "__all__" # we want all the field of our model
+        # fields = ("title", "description", "body") # choose certain value
 
-    def update(self, instance, validated_data):
-        instance.author = validated_data.get('author', instance.author)
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get(
-            'description', instance.description
-        )
-        instance.body = validated_data.get('body', instance.body)
-        instance.location = validated_data.get('location', instance.location)
-        instance.publication_date = validated_data.get(
-            'publication_date', instance.publication_date
-        )
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    def get_time_since_publication(self, object):
+        publication_date = object.publication_date
+        now = datetime.now()
+        time_delta = timesince(publication_date, now)
+        return time_delta
+
+    def validate(self, data):
+        if data["title"] == data["description"]:
+            raise serializers.ValidationError(
+                "Title and description must be different"
+            )
+        return data
+
+    def validate_title(self, data):
+        if len(data) < 30:
+            raise serializers.ValidationError(
+                "The title has to be at least 30 characters"
+            )
+        return data
+
+
+# class ArticleSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     author = serializers.CharField()
+#     title = serializers.CharField()
+#     description = serializers.CharField()
+#     body = serializers.CharField()
+#     location = serializers.CharField()
+#     publication_date = serializers.DateField()
+#     active = serializers.BooleanField()
+#     created_at = serializers.DateTimeField(read_only=True)
+#     updated_at = serializers.DateTimeField(read_only=True)
+
+#     def create(self, validated_data):
+#         print(validated_data)
+#         return Article.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get(
+#             'description', instance.description
+#         )
+#         instance.body = validated_data.get('body', instance.body)
+#         instance.location = validated_data.get('location', instance.location)
+#         instance.publication_date = validated_data.get(
+#             'publication_date', instance.publication_date
+#         )
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
